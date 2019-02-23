da7c06255572f24764b174e45caa72e742afdec5
Kirill Makhalov <zvookiejoo@gmail.com>
2019-02-23 12:41:15 +0500

    New visual styles enabled

--- Crawler/UI.h
+++ Crawler/UI.h
@@ -24,6 +24,7 @@ SOFTWARE.
 
 #pragma once
 #include "stdafx.h"
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='X86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 class UI
 {
